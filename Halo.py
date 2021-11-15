ed2aeac3803119ce309a660bd966d37105ee817d
deanirashlmt <94366588+deanirashlmt@users.noreply.github.com>
2021-11-15 21:16:51 +0700

    Create Halo.py

---
+++ Halo.py
@@ -0,0 +1,8 @@
+Nama = "Deanira Shelomita"
+NIM = "212410102053"
+Prodi = "Teknologi Informasi"
+Fakultas = "Ilmu Komputer"
+Asal = "Banyuwangi"
+Kelompok = 11
+
+print("Hai, kenalin namaku", Nama, "dengan NIM", NIM, "dari prodi", Prodi, "dari fakultas", Fakultas, "asal saya dari", Asal, "dari kelompok", Kelompok, "salam kenal semuanya")
